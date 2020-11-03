8d08af08137c64605e384804c5f9fb05cac32c08
Leo <Aqu4men@gmail.com>
2020-11-03 11:05:24 +0100

    cursed image + sprites emojis

--- Rookie Team Mist/Assets/T2Assets/scripts/T2Ennemies.cs
+++ Rookie Team Mist/Assets/T2Assets/scripts/T2Ennemies.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class T2Ennemies : MonoBehaviour
 {
+    [SerializeField] private float wait;
+    private void Start()
+    {
+        Destroy(gameObject,10);
+    }
     [SerializeField] private float speed;
     void Update()
     {
-        transform.Translate(0, Time.deltaTime * speed, 0);
+        if(wait > 0)
+        {
+            wait -= Time.deltaTime;
+        }
+        else
+        {
+            transform.Translate(0, Time.deltaTime * speed, 0);
+        }
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
+    
 }
