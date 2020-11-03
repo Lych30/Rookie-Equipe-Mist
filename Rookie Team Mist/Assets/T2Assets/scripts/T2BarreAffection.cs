6ad126f503f6ee83b24468aa21e3c74a29515ffd
illianes <pierremariotte98@mail.com>
2020-11-03 09:47:13 +0100

    Barre D'affection + Dégât ou heal

---
+++ Rookie Team Mist/Assets/T2Assets/scripts/T2BarreAffection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class T2BarreAffection : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public Gradient gradient;
+
+    public Image fill;
+
+    public void SetMaxAffection(int affection)
+    {
+        slider.maxValue = affection;
+        slider.value = affection;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetAffection(int affection)
+    {
+        slider.value = affection;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+    private void Update()
+    {
+
+    }
+}
---
+++ Rookie Team Mist/Assets/T2Assets/scripts/T2Degat.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class T2Degat : MonoBehaviour
+{
+    public int maxAffection = 100;
+    public int affection;
+    public T2BarreAffection BarreAffection;
+
+    private void Start()
+    {
+        BarreAffection.SetMaxAffection(maxAffection);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("T2MauvaisSmiley"))
+        {
+            Takedmg(20);
+        }
+        if (collision.gameObject.CompareTag("T2BonSmiley"))
+        {
+            Soin(20);
+        }
+    }
+
+    public void Soin(int heal)
+    {
+        affection += heal;
+        BarreAffection.SetAffection(affection);
+        if (affection >= maxAffection)
+            affection = maxAffection;
+    }
+
+    public void Takedmg(int dmg)
+    {
+        affection -= dmg;
+        BarreAffection.SetAffection(affection);
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    private void Update()
+    {
+
+    }
+}
