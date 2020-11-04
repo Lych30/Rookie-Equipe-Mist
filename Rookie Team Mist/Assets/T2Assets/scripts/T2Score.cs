6d1f82210b90c152406ab966929f4571f024097d
illianes <pierremariotte98@mail.com>
2020-11-04 17:43:07 +0100

    Score

---
+++ Rookie Team Mist/Assets/T2Assets/scripts/T2Score.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class T2Score : MonoBehaviour
+{
+    public Text scoreText;
+    private int score;
+
+
+
+    void Start()
+    {
+        score = 0;
+        SetScoreText();
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("T2BonSmiley"))
+        {
+            score = ++score;
+            SetScoreText();
+        }
+        if (collision.gameObject.CompareTag("T2MauvaisSmiley"))
+        {
+            score = --score;
+            SetScoreText();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            score = ++score;
+            SetScoreText();
+        }
+    }
+}
